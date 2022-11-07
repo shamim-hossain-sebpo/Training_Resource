d1746b63736094860e95b2b204912b6f5143cdfb
Md. Shamim Hossain <115537559+shamim-hossain-sebpo@users.noreply.github.com>
2022-11-07 21:23:52 +0600

    Add files via upload
    Updated Script

--- src/main/groovy/DemoScript/Boi_Securities.groovy
+++ src/main/groovy/DemoScript/Boi_Securities.groovy
@@ -20,13 +20,15 @@ class BoiSecurities {
     //a2d905c86b1424c8656d0432d41029977e4f81cd
     final def ocrReader
     final MAIN_URL = "https://www.ecfr.gov/current/title-15/subtitle-B/chapter-VII/subchapter-C/part-744/appendix-Supplement%20No.%204%20to%20Part%20744"
-    def entity;
     def allEntity = []
-    def entityName = []
+    def entityNameList = []
     def entityAlias = []
     def entityAddres = []
     def aliasMap = [:]
     def addressMap = [:]
+
+    def countWithAKA =0
+    def countwithountAKA = 0;
 
     BoiSecurities(context) {
         this.context = context
@@ -38,18 +40,13 @@ class BoiSecurities {
         //println html
         invokeEntity(html)
 
-        //----- printing All Info and Alias----
-        def data = aliasMap.entrySet();
-        data.each { entry -> println entry.key + "-----" +  entry.value
-        }
+        printMapValue()
 
-//        for(i in entityName){
-//            println i
-//        }
-
-        println aliasMap.size()
-        println "Total Entities ${entityName.size()}"
+        println "Scraping Final Data : "+aliasMap.size()
+        println "Total Entities ${entityNameList.size()}"
         println "Total Allentity ${allEntity.size()}"
+        println "Entity With a.k.a : ${countWithAKA}"
+        println "Entity Without a.k.a : ${countwithountAKA}"
 
     }
 
@@ -57,41 +54,59 @@ class BoiSecurities {
     def invokeEntity(def responseBody) {
         def entityData = responseBody =~ /(?is)<tr>.*?(?:<td.*?>(.*?)<\/td>\s*)(?:<td.*?>(.*?)<\/td>).+?<\/tr>/
         def count = 1;
-        def entityMatcher;
+        def entity = ""
+        //def entityMatcher;
+        def entityName;
 
         while (entityData.find()) {
             count++
             // --- Getting entity ---- //
             entity = entityData.group(2)
-             println entity
+             //println entity
             allEntity.push(entity)
 
             entityAlias.clear()
 
-            // -----Getting Name and Alias ----- //
-            if (!entity.contains("a.k.a")) {
-                entityMatcher = entity =~ /(?m)^.+?(?=,)/
-                if (entityMatcher) {
-                    entityName.push(entityMatcher.group())
-                    aliasMap.put(entityMatcher.group(),"No Alias!")
-                    addressMap.put(entityMatcher.group(),sanitizeAddress(entity, entityMatcher.group()))
-                }
-            } else {
-                // -----Getting Name and Alias ----- //
-                entityMatcher = entity =~ /(?m)^.+?(?=(?:\s*?<br>\s*?<br>))/
-                if (entityMatcher) {
-                    entityName.push(entityMatcher.group())
-                    String[] arrayNameAndAlias = sanitizeNameAndAlias(entityMatcher.group()).split("-")
 
-                    for (int i = 1; i < arrayNameAndAlias.length; i++) {
-                        entityAlias.push(arrayNameAndAlias[i])
+            // -----Getting Name Only----- //
+            if (!entity.contains("a.k.a")) {
+
+               def entityMatcher = entity =~ /(?m)^.+?(?=[,])/
+                if (entityMatcher) {
+
+                    entityName = entityMatcher.group()
+                    entityNameList.push(entityName)
+
+                    if(!aliasMap.containsKey(entityName)){
+                        countwithountAKA++
+                        aliasMap.put(entityName,entityAlias.add("No Alias!"))
+                        addressMap.put(entityName,sanitizeAddress(entity, entityName))
                     }
 
-                    aliasMap.put(arrayNameAndAlias[0], entityAlias)
-                    //println " entityAliasList :  $entityAlias"
+                }else{
+                    //println entity
+                }
+            } else {
 
+                // -----Getting Name and Alias ----- //
+              def entityMatcher = entity =~ /(?m)^.+?(?=(?:\s*?<br>\s*?<br>))/
+                if (entityMatcher) {
+                    countWithAKA++
+                    entityName = entityMatcher.group()
+                    //println entityName
+                    String[] arrayNameAndAlias = sanitizeNameAndAliasWithAKA(entityName).split("-")
+                    entityNameList.push(sanitizeName(arrayNameAndAlias[0]))
 
-                    addressMap.put(arrayNameAndAlias[0],sanitizeAddress(entity, entityMatcher.group()))
+                    for (int i = 1; i < arrayNameAndAlias.length; i++) {
+                        entityAlias.add(arrayNameAndAlias[i])
+                    }
+
+                    if(!aliasMap.containsKey(arrayNameAndAlias[0])){
+                        aliasMap.put(arrayNameAndAlias[0], entityAlias)
+//                    println " entityAliasList :  $entityAlias"
+                        addressMap.put(arrayNameAndAlias[0],sanitizeAddress(entity, entityName))
+                    }
+
                 }
             }
 
@@ -99,20 +114,32 @@ class BoiSecurities {
         println "count : ${count}"
     }
 
+    def printMapValue(){
+        //aliasMap.entrySet().each {entry-> println entry.key+"-------"+entry.value}
+        //addressMap.entrySet().each {entry-> println entry.key+"--------"+entry.value}
+
+//        for( i in entityNameList){
+//            println i
+//        }
+    }
+
 
     def sanitizeAddress(def entity, def entityName) {
-        def tempAddress = entity.replace(entityName, "");
+        def tempAddress = entity.replace(entityName, "")
         def tempMatcher;
         tempAddress = tempAddress.replace("<br>", "")
         tempAddress = tempAddress.replace("<em>", "")
         tempAddress = tempAddress.replace("</em>", "")
+        tempAddress.trim()
 
-        tempMatcher = tempAddress =~ /(?<=,\s).+/
+        tempMatcher = tempAddress =~ /(?<=[,\.]\s).{3,}/
         if (tempMatcher) {
             tempAddress = tempMatcher.group()
+          return  tempAddress
+        }else{
+           //return tempAddress="No Address!"
+            return tempAddress
         }
-
-        return tempAddress
 
     }
 
@@ -124,12 +151,34 @@ class BoiSecurities {
             tempAlias = nameAndAlias.replace(tempMatcher.group(), "")
         }
 
-        tempAlias = tempAlias.replace("<br>", "")
-        tempAlias = tempAlias.replace("<em>and</em>", "")
+        tempAlias.trim()
+        tempAlias = tempAlias.replaceAll(/<br>|<em>and<\/em>"/, "")
+        tempAlias = tempAlias.replaceAll(/&amp;/,"&")
 
 
         return tempAlias
     }
+
+    def sanitizeNameAndAliasWithAKA(def nameAndAlias){
+        def tempAlias="";
+        def tempMatcher;
+        tempMatcher = nameAndAlias =~ /a\.k\.a.+?(?=<br>)/
+        if (tempMatcher) {
+            tempAlias = nameAndAlias.replace(tempMatcher.group(), "")
+        }
+        tempAlias.trim()
+        tempAlias = tempAlias.replaceAll(/<br>|<em>and<\/em>"/, "")
+        tempAlias = tempAlias.replaceAll(/&amp;/,"&")
+
+
+        return tempAlias
+    }
+
+    def sanitizeName(String name){
+        name = name.replaceAll(/,$/,"").trim()
+        println name
+        return name
+re  }
 
 
 //------------------------------Misc utils part---------------------//
